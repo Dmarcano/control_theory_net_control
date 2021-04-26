e59a7a4ca4b35bcbdef0eccb489258b1f9768f47
Unknown <dmarcan1@swarthmore.edu>
2021-04-26 01:48:02 -0400

    scaffold backprop : learning_rate

--- libs/neural_net/src/lib.rs
+++ libs/neural_net/src/lib.rs
@@ -1,7 +1,7 @@
 mod activation_funcs;
 mod network_impl;
-use nalgebra::{DVector, RowDVector};
-use rand::{rngs::OsRng, RngCore};
+use nalgebra::RowDVector;
+use rand::{rngs::OsRng, Rng};
 
 use network_impl::matrix_impl::LayerMatrix;
 
@@ -15,6 +15,10 @@ pub struct NeuralNetwork {
     // that severely speed up the operations so we add a trait object to signify that we may use
     // multiple implementations of such object
     layers: Vec<LayerMatrix>,
+
+    pub learning_rate: f64,
+
+    pub lambda: f64,
 }
 
 /// A layer topology is simply the number of neurons per a single layer.
@@ -84,7 +88,7 @@ impl NeuralNetwork {
     /// The network does not automatically add a bias neuron and leaves
     /// it to users to augment input vectors to include bias terms in their dataset
     ///
-    pub fn random(topology: &[LayerTopology]) -> Self {
+    pub fn random(topology: &[LayerTopology], alpha: Option<f64>, lambda: Option<f64>) -> Self {
         // we want networks of at least 1 hidden layer
         assert!(topology.len() > 1);
         let mut rng = OsRng;
@@ -101,13 +105,26 @@ impl NeuralNetwork {
             })
             .collect();
 
-        NeuralNetwork { layers }
+        let learning_rate = match alpha {
+            None => rng.gen_range(0.0..=0.2),
+            Some(val) => val,
+        };
+        let regularization = match lambda {
+            None => rng.gen_range(0.0..=0.1),
+            Some(val) => val,
+        };
+
+        NeuralNetwork {
+            layers,
+            learning_rate,
+            lambda: regularization,
+        }
     }
 
     /// creates a brand new network using pre-determined weights given
     /// It assumes that the bias weight of each layer is included in the network topology, that is
     /// the neuron's individual bias are all summed to one bias neuron included in the input weights
-    pub fn load_weights(weights: Vec<LayerWeights>) -> Self {
+    pub fn load_weights(weights: Vec<LayerWeights>, alpha: f64, lambda: f64) -> Self {
         let mut built_layers = Vec::new();
 
         for layer_weights in weights {
@@ -116,6 +133,8 @@ impl NeuralNetwork {
         }
         NeuralNetwork {
             layers: built_layers,
+            learning_rate: alpha,
+            lambda,
         }
     }
 }
@@ -125,27 +144,23 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn new_random_test() {}
-
-    #[test]
-    fn new_from_weights_test() {
-        // unimplemented!();
-    }
-
-    #[test]
     fn propagation_test() {
         // normal prop
         let layer_one_weights = vec![vec![1.0, 2.0], vec![3.0, 4.0], vec![5.0, 6.0]];
         let layer_two_weights = vec![vec![0.5], vec![0.1]];
 
-        let net = NeuralNetwork::load_weights(vec![
-            LayerWeights {
-                weights: layer_one_weights,
-            },
-            LayerWeights {
-                weights: layer_two_weights,
-            },
-        ]);
+        let net = NeuralNetwork::load_weights(
+            vec![
+                LayerWeights {
+                    weights: layer_one_weights,
+                },
+                LayerWeights {
+                    weights: layer_two_weights,
+                },
+            ],
+            0.1,
+            0.1,
+        );
 
         let input = RowDVector::from_vec(vec![-0.5, 1.5, 2.0]);
         let expected = 8.7;
