47d21e813c185703c072db7ac807a57acf233abc
Diego Marcano <marcanp.diego@gmail.com>
2021-08-05 00:17:56 -0400

    cargo fmt

--- examples/mnist/src/main.rs
+++ examples/mnist/src/main.rs
@@ -1,15 +1,14 @@
 use mnist_load::load_mnist_from_path;
 
-mod display; 
-use display::to_ppm_file; 
+mod display;
+use display::to_ppm_file;
 
 fn main() -> Result<(), std::io::Error> {
     let path = "data/";
-    let data_set = load_mnist_from_path(path)?; 
+    let data_set = load_mnist_from_path(path)?;
 
     let out_path = "output/first.ppm";
     to_ppm_file(out_path, &data_set.train_set[0])?;
 
     Ok(())
 }
-
