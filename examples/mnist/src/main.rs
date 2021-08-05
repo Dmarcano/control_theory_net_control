416502a277c69e3171e32d4688aa4980bd790f9e
Diego Marcano <marcanp.diego@gmail.com>
2021-08-05 00:46:21 -0400

    scaffold learning mnist

--- examples/mnist/src/main.rs
+++ examples/mnist/src/main.rs
@@ -1,14 +1,34 @@
-use mnist_load::load_mnist_from_path;
+use mnist_load::{load_mnist_from_path, MnistDataset};
+use neural_net::{NeuralNetwork, LayerTopology, activation_funcs::ActivationFunction}; 
 
 mod display;
 use display::to_ppm_file;
 
 fn main() -> Result<(), std::io::Error> {
     let path = "data/";
-    let data_set = load_mnist_from_path(path)?;
+    let mnist_data = load_mnist_from_path(path)?;
 
-    let out_path = "output/first.ppm";
-    to_ppm_file(out_path, &data_set.train_set[0])?;
+    let mut nn = mnist_nn();
 
+    learning_epoch(&mut nn, &mnist_data);
     Ok(())
+}
+
+
+fn mnist_nn() -> NeuralNetwork { 
+    let layer_one = LayerTopology{num_neurons : 28*28};
+    let layer_two = LayerTopology{num_neurons: 300};
+    let layer_three = LayerTopology{num_neurons: 10}; 
+
+    NeuralNetwork::random(
+        &[layer_one, layer_two, layer_three],
+        Some(0.1), None,
+        ActivationFunction::LeakyRelu)
+}
+
+
+fn learning_epoch(nn : &mut NeuralNetwork, mnist_data : &MnistDataset) { 
+
+    unimplemented!()
+
 }
